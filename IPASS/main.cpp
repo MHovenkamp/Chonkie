b2447bab1e4463789e2417dada46eb28c02bc183
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2019-06-20 14:35:02 +0200

    Week 1 dag 4, werkende kleurensensor zonder hpp/cpp

--- IPASS/main.cpp
+++ IPASS/main.cpp
@@ -1,4 +1,3 @@
-#include <chrono>
 #include "hwlib.hpp"
 #include "../../hwlib/library/hwlib.hpp"
 
@@ -16,20 +15,27 @@ public:
         pin( rhs.pin )
     {}
     void sample(){
-        while(pin.read() < 4095);
-        while(pin.read() == 4095);
+        std::array<uint_fast32_t, 30> testsamples;
+        for(unsigned int i = 0; i < 30; i++){
+            testsamples[i] = pin.read();
+        }
+        uint_fast32_t high = 0;
+        for(unsigned int j = 0; j < 30; j++){
+            if( testsamples[j] > high ){
+                high = testsamples[j];
+            }
+        }
+        while(pin.read() < high);
+        while(pin.read() == high);
         t1 = hwlib::now_us();
-        while(pin.read() < 4095);
-        while(pin.read() == 4095);
+        while(pin.read() < high);
+        while(pin.read() == high);
         t2 = hwlib::now_us();
     }
 
     int calculate(){
         uint_fast64_t duration = t2-t1;
-        hwlib::cout << "duration" << duration << hwlib::endl;
-        int freqS = 1000000/duration;
-        frequenty = freqS;
-        hwlib::cout << "Frequenty: " << frequenty << hwlib::endl;
+        frequenty = duration;
         return frequenty;
     }
 };
@@ -43,6 +49,7 @@ private:
     int red = 0;
     int green = 0;
     int blue = 0;
+    char color = 'X';
 public:
     colorSensor( hwlib::target::pin_adc & freq, hwlib::target::pin_out & s0, hwlib::target::pin_out & s1, hwlib::target::pin_out & s2, hwlib::target::pin_out & s3 ):
         freqMeasure( freq ),
@@ -50,53 +57,177 @@ public:
         s1( s1 ),
         s2( s2 ),
         s3( s3 )
-    {}
-    void calculateRGB(){
-        //red
-        setRed();
-        flush();
-        sample();
-        red = calculate();
-        //Green
-        setGreen();
-        flush();
-        sample();
-        green = calculate();
-        //Blue
-        setBlue();
-        flush();
-        sample();
-        blue = calculate();
-    }
+    {s0.write(0);
+    s1.write(1);}
+
     void setRed(){
-        s0.write(1);
-        s1.write(1);
         s2.write(0);
         s3.write(0);
         flush();
     }
+
     void setGreen(){
-        s0.write(1);
-        s1.write(1);
         s2.write(1);
         s3.write(1);
         flush();
     }
+
     void setBlue(){
-        s0.write(1);
-        s1.write(1);
         s2.write(0);
         s3.write(1);
         flush();
     }
+
     void flush(){
         s0.flush();
         s1.flush();
         s2.flush();
         s3.flush();
     }
-};
 
+    int calculatecolor(){
+        const int sampleSize = 25;
+        std::array<int,sampleSize> samples;
+        for (unsigned int i = 0; i < sampleSize; i++){
+            sample();
+            samples[i]= calculate();
+        }
+        int value = 0;
+        for (unsigned int j = 0; j < sampleSize; j++){
+            value += samples[j];
+        }
+        return value/sampleSize;
+    }
+
+    void calculateRGB(){
+        //red
+        setRed();
+        flush();
+        red = calculatecolor();
+        //Green
+        setGreen();
+        flush();
+        green = calculatecolor();
+        //Blue
+        setBlue();
+        flush();
+        blue = calculatecolor();
+        easyUp();
+    }
+
+    void easyUp(){
+        hwlib::cout << "_______________________________" << hwlib::endl;
+        int highest = red;
+        int lowest = blue;
+        int last = 0;
+        if( red > green && red > blue ){
+            highest = red;
+        }
+        if( green > red && green > blue ){
+            highest = green;
+        }
+        if( blue > green && blue > red ){
+            highest = blue;
+        }
+        if( red < green && red < blue ){
+            lowest = red;
+        }
+        if( green < red && green < blue ){
+            lowest = green;
+        }
+        if( blue < green && blue < red ){
+            lowest = blue;
+        }
+        while( lowest > 0 ){
+            red = red/2;
+            green = green/2;
+            blue = blue/2;
+            if( red < green && red < blue ){
+                lowest = red;
+            }
+            if( green < red && green < blue ){
+                lowest = green;
+            }
+            if( blue < green && blue < red ){
+                lowest = blue;
+            }
+            if( red > green && red > blue ){
+                highest = red;
+            }   
+            if( green > red && green > blue ){
+                highest = green;
+            }
+            if( blue > green && blue > red ){
+                highest = blue;
+            }
+            if (lowest == last || highest < 255){
+                break;
+            }
+            last = lowest;
+        }   
+    }
+
+    void printRGB(){
+        hwlib::cout << "Red: " << red << hwlib::endl;
+        hwlib::cout << "Green: " << green << hwlib::endl;
+        hwlib::cout << "Blue: " << blue << hwlib::endl;
+        hwlib::cout << "Color " << color << hwlib::endl;
+    }
+
+    void nameColor(){
+        std::array<int,2> zone1 = {0,32};
+        std::array<int,2> zone2 = {32,64};
+        std::array<int,2> zone3 = {64,96};
+        std::array<int,2> zone4 = {96,128};
+        std::array<int,2> zone5 = {128,160};
+        std::array<int,2> zone6 = {160,192};
+        std::array<int,2> zone7 = {192,224};
+        std::array<int,2> zone8 = {224,255};
+        std::array<std::array<int,2>,8> zones = {zone1, zone2, zone3, zone4, zone5, zone6, zone7, zone8};
+        int redZone = 0;
+        int greenZone = 0;
+        int blueZone = 0;
+        for( unsigned int i = 0; i < 8; i++ ){
+            if( red >= zones[i][0] && red <= zones[i][1]){
+                redZone = i+1;
+            }
+            if( green >= zones[i][0] && green <= zones[i][1]){
+                greenZone = i+1;
+            }
+            if( blue >= zones[i][0] && blue <= zones[i][1]){
+                blueZone = i+1;
+            }
+        }
+        if( redZone == 2 || redZone == 3 ){
+            if( greenZone == 5 || greenZone == 6 ){
+                if( blueZone == 2 || blueZone == 3 ){
+                    color = 'R';
+                }
+            }
+        }
+        if( redZone == 6 || redZone == 7 ){
+            if( greenZone == 5 || greenZone == 6 ){
+                if( blueZone == 4 || blueZone == 5 ){
+                    color = 'G';
+                }
+            }
+        }
+        if( redZone == 5 || redZone == 6 ){
+            if( greenZone == 4 || greenZone == 5 ){
+                if( blueZone == 2 || blueZone == 3 ){
+                    color = 'B';
+                }
+            }
+        }
+        if( redZone == 5 || redZone == 6 ){
+            if( greenZone == 6 || greenZone == 7 ){
+                if( blueZone == 7 || blueZone == 8 ){
+                    color = 'Y';
+                }
+            }
+        }
+    }
+};
 
 int main( void ){
     hwlib::wait_ms(100);
@@ -107,18 +238,11 @@ int main( void ){
     auto frequenty = hwlib::target::pin_adc( hwlib::target::ad_pins::a5 );
     //freqMeasure pinIn( frequenty );
     colorSensor sensor( frequenty , s0, s1, s2, s3 );
-    hwlib::cout << "red" << hwlib::endl;
-    sensor.setRed();
-    sensor.sample();
-    sensor.calculate();
-    hwlib::cout << "green" << hwlib::endl;
-    sensor.setGreen();
-    sensor.sample();
-    sensor.calculate();
-    hwlib::cout << "blue" << hwlib::endl;
-    sensor.setBlue();
-    sensor.sample();
-    sensors.calculate();
+    for(;;){
+        sensor.calculateRGB();
+        sensor.nameColor();
+        sensor.printRGB();
+    }
 }
 
 
